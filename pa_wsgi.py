0f5e534e76318388f14c222fb82fb7ea266fec79
Jorge Escobar <jorge@fromzero.io>
2018-04-16 15:34:12 -0400

    Debug set to True on PA WSGI

--- pa_wsgi.py
+++ pa_wsgi.py
@@ -4,6 +4,8 @@ path = '/home/' + python_anywhere_username + '/opt/simple_flask_app'
 if path not in sys.path:
     sys.path.append(path)
 
+from hello import app as application
+
 from hello import app
 app.debug = True
 
