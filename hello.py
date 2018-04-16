2f7ea6d5031c7706fe39e1ea734887cd0d315d8a
Jorge Escobar <jorge@fromzero.io>
2018-04-16 14:48:38 -0400

    WSGI for PA with Debug mode

--- hello.py
+++ hello.py
@@ -1,6 +1,5 @@
 from flask import Flask
 app = Flask(__name__)
-app.config['DEBUG'] = True
 
 @app.route('/')
 def hello_world():
--- pa_wsgi.py
+++ pa_wsgi.py
@@ -4,4 +4,8 @@ path = '/home/' + python_anywhere_username + '/opt/simple_flask_app'
 if path not in sys.path:
     sys.path.append(path)
 
-from hello import app as application
+from hello import app
+app.debug = True
+
+from werkzeug.debug import DebuggedApplication
+application = DebuggedApplication(app, evalex=True)
