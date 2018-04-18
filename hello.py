1eeb53b945775122f04f09606f615d0a8ab7c5d9
Jorge Escobar <jorge@fromzero.io>
2018-04-18 08:28:41 -0400

    step-7

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Index Page'
+    return render_template('index.html')
 
 @app.route('/hello/<int:planet>')
 def hello(planet):
