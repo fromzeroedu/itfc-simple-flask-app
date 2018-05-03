b1ae1e0be7bcd5e546da9629f4e5168b79e81d2a
Jorge Escobar <jorge@fromzero.io>
2018-05-03 08:06:56 -0400

    step-18

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,3 +8,9 @@ def index():
 @app.route('/hello/<int:planet>')
 def hello(planet):
     return render_template('hello.html', t_planet=planet)
+
+@app.route('/form', methods=['GET'])
+def form():
+    first_name = request.args['first_name']
+    last_name = request.args['last_name']
+    return f'First Name: {first_name}, Last Name: {last_name}'
