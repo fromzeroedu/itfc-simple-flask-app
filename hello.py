1c57f1ae070f31d43d039225e89b465deaad25ef
Jorge Escobar <jorge@fromzero.io>
2018-05-10 10:19:05 -0400

    step-26

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
 
-app.secret_key = 'You Will Never Guess'
+app.config.from_pyfile('settings.py')
 
 @app.route('/')
 def index():
