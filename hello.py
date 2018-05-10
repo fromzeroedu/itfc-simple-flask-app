93d6e5a1b75b36f9a89813b8b593c16009e293b0
Jorge Escobar <jorge@fromzero.io>
2018-05-10 07:59:58 -0400

    step-24

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, make_response
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,9 +14,12 @@ def form():
     if request.method == 'POST':
         first_name = request.values.get('first_name')
         last_name = request.values.get('last_name')
-        return redirect(url_for('registered'))
+        response = make_response(redirect(url_for('registered')))
+        response.set_cookie('first_name', 'John')
+        return response
     return render_template('form.html')
 
 @app.route('/thank_you')
 def registered():
-    return 'Thank you!'
+    first_name = request.cookies.get('first_name')
+    return f'Thank you, {first_name}!'
