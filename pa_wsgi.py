50458c24bd94c2c3469b9493f4e703165cc9f779
Jorge Escobar <jorge@fromzero.io>
2018-05-15 17:46:12 -0400

    step-27 wsgi for pa

--- pa_wsgi.py
+++ pa_wsgi.py
@@ -1,8 +1,12 @@
-import sys
+import os, sys
+from dotenv import load_dotenv
+
 python_anywhere_username = 'jorge3'
 path = '/home/' + python_anywhere_username + '/opt/simple_flask_app'
 if path not in sys.path:
     sys.path.append(path)
+
+load_dotenv(os.path.join(path, '.flaskenv'))
 
 from hello import app
 app.debug = True
